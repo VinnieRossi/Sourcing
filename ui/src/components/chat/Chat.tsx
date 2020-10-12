c48d7e77e795edd37acfbb39c9f62bd4c6a38c8e
Vinnie <vinnie.rossi12@gmail.com>
2020-10-12 10:23:05 -0400

    Update Chat.tsx

--- ui/src/components/chat/Chat.tsx
+++ ui/src/components/chat/Chat.tsx
@@ -32,6 +32,7 @@ const Chat = () => {
                 })
                 .catch((e: any) => console.log('Connection failed: ', e));
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [connection]);
 
     const setupConnectionListeners = () => {
